0aba3a81ee284273908978bd0fefd383a0b4bf38
Kauan677 <87682301+Kauan677@users.noreply.github.com>
2021-07-27 13:56:25 -0300

    Create Estrutura de Repetição.py

---
+++ Estrutura de Repetição.py
@@ -0,0 +1,12 @@
+#for c in range('') = Estrutura de repetição!
+from time import sleep
+
+print('-=-' * 16)
+print('CONTAGEM REGRESSIVA PARA OS FOGOS DE ARTIFICIO!')
+print('-=-' * 16)
+sleep(5)
+n = 0
+for c in range(10, n-1, -1):
+    print(c)
+    sleep(2)
+print('FELIZ ANO NOVO!!!')
