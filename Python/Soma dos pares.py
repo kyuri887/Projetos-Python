435b035ca287bb48193bfaf54a4ec3e8a5bd2846
Kauan677 <87682301+Kauan677@users.noreply.github.com>
2021-07-30 15:31:20 -0300

    Soma dos pares

---
+++ Python/Soma dos pares.py
@@ -0,0 +1,8 @@
+soma = 0
+cont = 0
+for c in range(0, 6):
+    par = int(input('Digite um número: '))
+    if par % 2 == 0:
+        soma += par
+        cont += 1
+print(f'Você informou {cont} números pares e a soma desses números é igual a {soma}')
