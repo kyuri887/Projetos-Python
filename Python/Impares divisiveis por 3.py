0df1fb203d2d2586947fc980d9f62cdfb0ed217d
Kauan677 <87682301+Kauan677@users.noreply.github.com>
2021-07-30 19:33:12 -0300

    .

--- Python/Impares divisiveis por 3.py
+++ Python/Impares divisiveis por 3.py
@@ -2,7 +2,7 @@ soma = 0
 cont = 0
 for c in range(1, 501, 2):
     if c % 3 == 0:
-        soma += c
         cont += 1
+        soma += c
 print(f'A soma de todos os {cont} valores solicitados é {soma}')
 
