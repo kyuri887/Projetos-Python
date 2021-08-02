2eadc77b4edc5c0813169f8fdf2d97968b290fe9
Kauan677 <87682301+Kauan677@users.noreply.github.com>
2021-08-02 01:38:04 -0300

    Tabuada

--- Python/Tabuada.py
+++ Python/Tabuada.py
@@ -1,3 +1,19 @@
-num = int(input('Digite um número para ver sua tabuada: '))
-for c in range(0, 11):
-    print(f'{num} x {c:2} = {num*c}')
+from time import sleep
+def tabuada():
+    num = int(input('Digite um número para ver sua tabuada: '))
+    print('PROCESSANDO...')
+    sleep(2)
+    for c in range(0, 11):
+        print(f'{num} x {c:2} = {num*c}')
+while True:
+    retorne = tabuada()
+    print('''Quer consultar novamente?
+    [ 1 ]Sim
+    [ 2 ]Não''')
+    retorne = int(input('Escolha uma opção: '))
+    if retorne == 1:
+        pass
+    elif retorne == 2:
+        break
+    else:
+        break
